057f3fb43da19ce0706ad96e991cc630a85a982a
Cchughiefe-art <cchughiefegmail.com>
2026-09-16 13:10:31 +0100

    Add batch episode downloads

--- launcher.ts
+++ launcher.ts
@@ -3,4 +3,5 @@ import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
 require("./src/core/launch-upgrades.js");
+require("./src/core/batch-downloads.js");
 require("./src/bot.js");
